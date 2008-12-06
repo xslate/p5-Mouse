ddca6c8f18c9c9fb84c2db836e3227f23aba3647
Tokuhiro Matsuno <tokuhirom@gp.ath.cx>
2008-12-06 05:40:43 +0000

    added benchmark script

---
+++ author/benchmark.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+use Benchmark qw/cmpthese/;
+use String::TT qw/tt/;
+
+for my $klass (qw/Moose Mouse/) {
+    eval tt(q{
+        package [% klass %]One;
+        use [% klass %];
+        has n => (
+            is => 'rw',
+            isa => 'Int',
+        );
+        no [% klass %];
+        __PACKAGE__->meta->make_immutable;
+    });
+    die $@ if $@;
+}
+
+print "---- new\n";
+cmpthese(
+    100000 => {
+        map { my $x = $_; $_ => sub { $x->new(n => 3) } }
+        map { "${_}One" }
+        qw/Moose Mouse/
+    }
+);
+
+print "---- new,set\n";
+cmpthese(
+    100000 => {
+        map { my $y = $_; $_ => sub { $y->new(n => 3)->n(5) } }
+        map { "${_}One" }
+        qw/Moose Mouse/
+    }
+);
