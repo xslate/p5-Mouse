3ef6ae567ed906e08036d71b747e30262b011ca1
Shawn M Moore <sartak@gmail.com>
2009-07-01 12:56:39 -0400

    Transfer copyright to II

--- lib/Mouse.pm
+++ lib/Mouse.pm
@@ -510,7 +510,9 @@ L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Mouse>.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008 Shawn M Moore.
+Copyright 2008-2009 Infinity Interactive, Inc.
+
+http://www.iinteractive.com/
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
