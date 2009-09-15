b5767a3a9505e854df8677a571a4d29dcc425c71
gfx <gfuji@cpan.org>
2009-09-15 12:51:11 +0900

    Move MethodAccessor stuff to Attribute

--- lib/Mouse/Meta/Attribute.pm
+++ lib/Mouse/Meta/Attribute.pm
@@ -6,7 +6,6 @@ require overload;
 use Carp 'confess';
 use Scalar::Util ();
 use Mouse::Meta::TypeConstraint;
-use Mouse::Meta::Method::Accessor;
 
 sub new {
     my ($class, $name, %options) = @_;
@@ -62,6 +61,111 @@ sub inlined_name {
     my $key   = "'" . $name . "'";
     return $key;
 }
+
+sub generate_accessor_method_inline {
+    my ($attribute) = @_;
+
+    my $name          = $attribute->name;
+    my $default       = $attribute->default;
+    my $constraint    = $attribute->type_constraint;
+    my $builder       = $attribute->builder;
+    my $trigger       = $attribute->trigger;
+    my $is_weak       = $attribute->is_weak_ref;
+    my $should_deref  = $attribute->should_auto_deref;
+    my $should_coerce = $attribute->should_coerce;
+
+    my $compiled_type_constraint    = $constraint ? $constraint->{_compiled_type_constraint} : undef;
+
+    my $self  = '$_[0]';
+    my $key   = $attribute->inlined_name;
+
+    my $accessor = 
+        '#line ' . __LINE__ . ' "' . __FILE__ . "\"\n" .
+        "sub {\n";
+    if ($attribute->_is_metadata eq 'rw') {
+        $accessor .= 
+            '#line ' . __LINE__ . ' "' . __FILE__ . "\"\n" .
+            'if (scalar(@_) >= 2) {' . "\n";
+
+        my $value = '$_[1]';
+
+        if ($constraint) {
+            if ($should_coerce) {
+                $accessor .=
+                    "\n".
+                    '#line ' . __LINE__ . ' "' . __FILE__ . "\"\n" .
+                    'my $val = Mouse::Util::TypeConstraints->typecast_constraints("'.$attribute->associated_class->name.'", $attribute->{type_constraint}, '.$value.');';
+                $value = '$val';
+            }
+            if ($compiled_type_constraint) {
+                $accessor .= 
+                    "\n".
+                    '#line ' . __LINE__ . ' "' . __FILE__ . "\"\n" .
+                    'unless ($compiled_type_constraint->('.$value.')) {
+                        $attribute->verify_type_constraint_error($name, '.$value.', $attribute->{type_constraint});
+                    }' . "\n";
+            } else {
+                $accessor .= 
+                    "\n".
+                    '#line ' . __LINE__ . ' "' . __FILE__ . "\"\n" .
+                    'unless ($constraint->check('.$value.')) {
+                        $attribute->verify_type_constraint_error($name, '.$value.', $attribute->{type_constraint});
+                    }' . "\n";
+            }
+        }
+
+        # if there's nothing left to do for the attribute we can return during
+        # this setter
+        $accessor .= 'return ' if !$is_weak && !$trigger && !$should_deref;
+
+        $accessor .= $self.'->{'.$key.'} = '.$value.';' . "\n";
+
+        if ($is_weak) {
+            $accessor .= 'Scalar::Util::weaken('.$self.'->{'.$key.'}) if ref('.$self.'->{'.$key.'});' . "\n";
+        }
+
+        if ($trigger) {
+            $accessor .= '$trigger->('.$self.', '.$value.');' . "\n";
+        }
+
+        $accessor .= "}\n";
+    }
+    else {
+        $accessor .= 'Carp::confess("Cannot assign a value to a read-only accessor") if scalar(@_) >= 2;' . "\n";
+    }
+
+    if ($attribute->is_lazy) {
+        $accessor .= $self.'->{'.$key.'} = ';
+
+        $accessor .= $attribute->has_builder
+                ? $self.'->$builder'
+                    : ref($default) eq 'CODE'
+                    ? '$default->('.$self.')'
+                    : '$default';
+        $accessor .= ' if !exists '.$self.'->{'.$key.'};' . "\n";
+    }
+
+    if ($should_deref) {
+        if (ref($constraint) && $constraint->name =~ '^ArrayRef\b') {
+            $accessor .= 'if (wantarray) {
+                return @{ '.$self.'->{'.$key.'} || [] };
+            }';
+        }
+        else {
+            $accessor .= 'if (wantarray) {
+                return %{ '.$self.'->{'.$key.'} || {} };
+            }';
+        }
+    }
+
+    $accessor .= 'return '.$self.'->{'.$key.'};
+    }';
+
+    my $sub = eval $accessor;
+    Carp::confess($@) if $@;
+    return $sub;
+}
+
 
 sub generate_predicate {
     my $attribute = shift;
@@ -139,9 +243,7 @@ sub create {
 
     # install an accessor
     if ($attribute->_is_metadata eq 'rw' || $attribute->_is_metadata eq 'ro') {
-        my $code = Mouse::Meta::Method::Accessor->generate_accessor_method_inline(
-            $attribute,
-        );
+        my $code = $attribute->generate_accessor_method_inline();
         $class->add_method($name => $code);
     }
 
