7f42bc912b90ff52a325b88709947f83736575d3
gfx <gfuji@cpan.org>
2009-09-28 10:21:21 +0900

    Add author/test-externals.pl to test external distributions that depend on Mouse

---
+++ author/test-externals.pl
@@ -0,0 +1,37 @@
+#!perl -w
+use strict;
+use FindBin qw($Bin);
+use autodie;
+
+my %dist = (
+    'HTTP-Engine' => q{git://github.com/http-engine/HTTP-Engine.git},
+    'Ark'         => q{git://github.com/typester/ark-perl.git},
+);
+
+my $distdir = 'externals';
+
+chdir $Bin;
+mkdir $distdir if not -e $distdir;
+
+$ENV{ANY_MOOSE} = 'Mouse';
+
+while(my($name, $repo) = each %dist){
+    chdir "$Bin/$distdir";
+
+    print "Go $name ($repo)\n";
+
+    if(!(-e "$name")){
+        system "git clone $repo $name";
+        chdir $name;
+    }
+    else{
+        chdir $name;
+        system "git pull";
+    }
+
+    print "$^X Makefile.PL\n";
+    system("$^X Makefile.PL") != 0 or next;
+
+    print "make test\n";
+    system "make test";
+}
