25f83ae1ee4aeed4ada879ffbd31a25b269efda8
gfx <gfuji@cpan.org>
2009-09-30 22:00:37 +0900

    A tiny tweak

--- lib/Mouse/Meta/Class.pm
+++ lib/Mouse/Meta/Class.pm
@@ -409,7 +409,7 @@ sub does_role {
         || $self->throw_error("You must supply a role name to look for");
 
     for my $class ($self->linearized_isa) {
-        my $meta = Mouse::Meta::Module::class_of($class);
+        my $meta = Mouse::Meta::Module::get_metaclass_by_name($class);
         next unless $meta && $meta->can('roles');
 
         for my $role (@{ $meta->roles }) {
--- lib/Mouse/Util/TypeConstraints.pm
+++ lib/Mouse/Util/TypeConstraints.pm
@@ -8,7 +8,7 @@ use Carp qw(confess);
 use Scalar::Util qw/blessed looks_like_number openhandle/;
 
 use Mouse::Util qw(does_role not_supported);
-use Mouse::Meta::Module; # class_of
+use Mouse::Meta::Module; # get_metaclass_by_name()
 use Mouse::Meta::TypeConstraint;
 
 our @ISA    = qw(Exporter);
@@ -221,7 +221,7 @@ sub _find_or_create_regular_type{
 
     return $TYPE{$spec} if exists $TYPE{$spec};
 
-    my $meta  = Mouse::Meta::Module::class_of($spec);
+    my $meta  = Mouse::Meta::Module::get_metaclass_by_name($spec);
 
     if(!$meta){
         return;
