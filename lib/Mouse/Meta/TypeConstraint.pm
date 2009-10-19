c4b28dd24bdccc23e62596b1b2a18b4df26d76dd
gfx <gfuji@cpan.org>
2009-10-19 18:26:50 +0900

    Fix subtyping issue

--- lib/Mouse/Meta/TypeConstraint.pm
+++ lib/Mouse/Meta/TypeConstraint.pm
@@ -101,11 +101,11 @@ sub compile_type_constraint{
     my @checks;
     for(my $parent = $self->parent; defined $parent; $parent = $parent->parent){
          if($parent->{hand_optimized_type_constraint}){
-            push @checks, $parent->{hand_optimized_type_constraint};
+            unshift @checks, $parent->{hand_optimized_type_constraint};
             last; # a hand optimized constraint must include all the parents
         }
         elsif($parent->{constraint}){
-            push @checks, $parent->{constraint};
+            unshift @checks, $parent->{constraint};
         }
     }
 
