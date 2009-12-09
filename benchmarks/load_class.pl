b33887a25e3ed857402a0d8c1f647927085ab8d6
gfx <gfuji@cpan.org>
2009-12-09 20:55:43 +0900

    Tidy

--- benchmarks/load_class.pl
+++ benchmarks/load_class.pl
@@ -10,10 +10,10 @@ print "Class::MOP $Class::MOP::VERSION\n";
 print "Mouse      $Mouse::VERSION\n";
 
 cmpthese -1 => {
-    'Class::MOP::load_class' => sub{
+    'Class::MOP' => sub{
         Class::MOP::load_class('Class::MOP::Class');
     },
-    'Mouse::Util::load_class' => sub{
+    'Mouse' => sub{
         Mouse::Util::load_class('Class::MOP::Class');
     },
 };
