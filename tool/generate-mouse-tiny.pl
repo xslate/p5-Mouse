c10b6ba9524e57ee153bfc280cd1548aa5ee1f20
gfx <gfuji@cpan.org>
2010-02-05 18:15:09 +0900

    Make Mouse::Tiny smaller

--- tool/generate-mouse-tiny.pl
+++ tool/generate-mouse-tiny.pl
@@ -54,6 +54,8 @@ for my $file (uniq
     $contents =~ s/__END__\b.*//s;          # remove documentation
     $contents =~ s/1;\n*$//;                # remove success indicator
 
+    $contents =~ s{^( (?:[ ]{4})+ )}{ "\t" x (length($1) / 4) }xmsge; # spaces to tabs
+
     $mouse_tiny .= "BEGIN{ # $file\n";
     $mouse_tiny .= $contents;
     $mouse_tiny .= "}\n";
@@ -71,7 +73,7 @@ EOF
 print { $handle } << 'EOF';
 # if regular Mouse is loaded, bail out
 unless ($INC{'Mouse.pm'}) {
-    # tell Perl we already have all of the Mouse files loaded:
+# tell Perl we already have all of the Mouse files loaded:
 EOF
 
 for my $file (@files) {
