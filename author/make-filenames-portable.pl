37a972b76454d93772ad4f1a98ad51b4173dfdb9
Fuji, Goro <gfuji@cpan.org>
2010-09-24 14:54:20 +0900

    Rename non-portable filenames

---
+++ author/make-filenames-portable.pl
@@ -0,0 +1,9 @@
+use strict;
+
+rename 't-failing/060_compat/004_extends_nonmoose_that_isa_moose_with_metarole.t',
+    't-failing/060_compat/004_entimwm' or warn $!;
+rename 't/050_metaclasses/041_moose_nonmoose_moose_chain_init_meta.t',
+    't/050_metaclasses/041_mnmcim.t' or warn $!;
+
+rename 't/600_todo_tests/001_exception_reflects_failed_constraint.t',
+    't/600_todo_tests/001_erfc.t' or warn $!;
