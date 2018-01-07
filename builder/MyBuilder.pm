4b6d8e49b6c4c916301a6bc4737b10cfc3479b2a
Shoichi Kaji <skaji@cpan.org>
2018-01-07 23:14:04 +0900

    revise MyBuilder.pm

--- builder/MyBuilder.pm
+++ builder/MyBuilder.pm
@@ -1,62 +1,51 @@
 package builder::MyBuilder;
 use strict;
 use warnings;
-use utf8;
-use 5.008_005;
 use base qw(Module::Build::XSUtil);
 
 sub new {
     my ($class, %args) = @_;
-
     $class->SUPER::new(
         %args,
-        conflicts => {
-            'Any::Moose',               '< 0.10',
-            'MouseX::AttributeHelpers', '< 0.06',
-            'MouseX::NativeTraits',     '< 1.00',
-        },
-        generate_ppport_h => 'ppport.h',
-        generate_xshelper_h => 'xshelper.h',
-        xs_files => {
-            'xs-src/Mouse.xs' => 'lib/Mouse.xs',
-        },
-        c_source => [
-            'xs-src'
-        ],
-        include_dirs => ['.'],
+        c_source => [ 'xs-src' ],
+        generate_ppport_h => 'xs-src/ppport.h',
+        generate_xshelper_h => 'xs-src/xshelper.h',
+        xs_files => { 'xs-src/Mouse.xs' => 'lib/Mouse.xs' },
     );
 }
 
 sub ACTION_code {
-    my ($self) = @_;
+    my ($self, @args) = @_;
 
-    system($^X, 'tool/generate-mouse-tiny.pl', 'lib/Mouse/Tiny.pm') == 0
-        or warn "Cannot generate Mouse::Tiny: $!";
+    $self->run_perl_script('tool/generate-mouse-tiny.pl', [], ['lib/Mouse/Tiny.pm']) or die;
 
-    open my $fh, '>', 'xs-src/xs_version.h';
-    print {$fh} "#ifndef XS_VERSION\n";
-    printf {$fh} "#define XS_VERSION \"%s\"\n", $self->dist_version;
-    print {$fh} "#endif\n";
-    close($fh);
-
-    unless ($self->pureperl_only) {
-        for my $xs (qw(
+    if (!$self->pureperl_only) {
+        $self->_write_xs_version;
+        my @xs = qw(
             xs-src/MouseAccessor.xs
             xs-src/MouseAttribute.xs
             xs-src/MouseTypeConstraints.xs
             xs-src/MouseUtil.xs
-        )) {
+        );
+        for my $xs (@xs) {
             (my $c = $xs) =~ s/\.xs\z/.c/;
             next if $self->up_to_date($xs, $c);
             $self->compile_xs($xs, outfile => $c);
         }
     }
+    $self->SUPER::ACTION_code(@args);
+}
 
-    $self->SUPER::ACTION_code();
+sub _write_xs_version {
+    my $self = shift;
+    open my $fh, '>', 'xs-src/xs_version.h' or die;
+    print  {$fh} "#ifndef XS_VERSION\n";
+    printf {$fh} "#define XS_VERSION \"%s\"\n", $self->dist_version;
+    print  {$fh} "#endif\n";
 }
 
 sub ACTION_test {
-    my ($self) = @_;
+    my ($self, @args) = @_;
 
     if ($ENV{COMPAT_TEST}) {
         $self->depends_on('moose_compat_test');
@@ -65,23 +54,20 @@ sub ACTION_test {
     if (!$self->pureperl_only) {
         local $ENV{MOUSE_XS} = 1;
         $self->log_info("xs tests.\n");
-        $self->SUPER::ACTION_test();
+        $self->SUPER::ACTION_test(@args);
     }
 
     {
         local $ENV{PERL_ONLY} = 1;
         $self->log_info("pureperl tests.\n");
-        $self->SUPER::ACTION_test();
+        $self->SUPER::ACTION_test(@args);
     }
 }
 
 sub ACTION_moose_compat_test {
-    my $class = shift;
-
-    $class->depends_on('code');
-
-    system($^X, 'tool/create-moose-compatibility-tests.pl')
-        == 0 or warn "tool/create-moose-compatibility-tests.pl: $!";
+    my $self = shift;
+    $self->depends_on('code');
+    $self->run_perl_script('tool/create-moose-compatibility-tests.pl') or die;
 }
 
 1;
